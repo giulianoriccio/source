1766c1b240014950343f36cf3f0cef61da72e6fd
giuliano riccio <giuliano.riccio@rifraf.it>
2018-01-19 14:16:20 +0100

    added java file

---
+++ vendor/vendor2/vendor3/VendorClass3.java
@@ -0,0 +1,14 @@
+public class VendorClass3
+{
+    public void publicVendorMethod3()
+    {
+    }
+
+    protected void protectedVendorMethod3()
+    {
+    }
+
+    private void privateVendorMethod3()
+    {
+    }
+}
